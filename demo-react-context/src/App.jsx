29fc697b1db9d1be501a190f7dcea7aef017a2e3
nguyentruonggiang212001 <nguyentruonggiang212001@gmail.com>
2024-12-09 19:56:02 +0700

    Main

--- demo-react-context/src/App.jsx
+++ demo-react-context/src/App.jsx
@@ -1,20 +1,7 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import HomePage from "./pages/HomePage";
-import { TodoProvider } from "./contexts/TodoContext";
-import TodoForm from "./pages/TodoForm";
 
 function App() {
-	return (
-		<>
-			<TodoProvider>
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/add" element={<TodoForm />} />
-				</Routes>
-			</TodoProvider>
-		</>
-	);
+  return <></>;
 }
 
 export default App;
